1569a9e630ea771900af56cf7f99d276cb2cc342
piyushasthana123 <piyushasthana444@gmail.com>
2026-03-21 15:54:29 +0530

    nav update 3

---
+++ form_detect/src/app/components/Guest/navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function navbar() {
+    return (
+        <div>
+
+            <div className='bg-gray-800 p-2 h-15'>
+                <div className='flex justify-between text-1xl font-bold'>
+                    PLANT DISEASE DETECTION
+                    <div className=''>
+                        <ul className='decoration-0 flex p-2 gap-4'>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact us</li>
+                            <li>Plants</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+        </div>
+    )
+}
+
+export default navbar
