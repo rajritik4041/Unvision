4308f82c20275f8a7ffe560b4f4333956330b3a0
piyushasthana123 <piyushasthana444@gmail.com>
2026-03-21 22:45:09 +0530

    nav completed

--- form_detect/src/app/Components/navbar/page.jsx
+++ form_detect/src/app/Components/navbar/page.jsx
@@ -6,34 +6,49 @@ import Link from 'next/link'
 function navbar() {
     const [isOpen, setIsOpen] = React.useState(false);
     const Navbar = [
-        { id: 1, name: "Home", link: "/home" },
-        { id: 2, name: "About", link: "/about" },
-        { id: 3, name: "Contact Us", link: "/contact us" },
-        { id: 4, name: "Plants", link: "/plants" },
+        { id: 1, name: "Home", link: "/" },
+        { id: 2, name: "About", link: "/aboutus" },
+        { id: 3, name: "Contact Us", link: "/contactus" },
+        { id: 4, name: "Opertunities", link: "/opertunities" },
+    ]
+    const btnname = [
+        {
+            id: 1,
+            name: "Sign Up",
+            link: "/signup"
+        },
+        {
+            id: 2,
+            name: "Login",
+            link: "/login"
+        }
     ]
     return (
         <div>
             <div className='bg-green-400   p-2 h-20 w-auto text-white' >
-                <div className='flex   justify-between items-center text-1xl h-full '>
+                <div className='flex   justify-between items-center text-[20px] h-full '>
                     <div> <h2 className='font-bold p-1'> PLANT DISEASE DETECTION</h2></div>
                     <div className='display-none '>
                         <ul className='decoration-0 flex w-full   items-center  m-1' >
-                            <li > <Link className='hover:text-[18px] px-2 ' href="/">Home</Link></li>
-                            <li ><Link className='hover:text-[18px] px-2 ' href="/aboutus">About</Link></li>
-                            <li ><Link className='hover:text-[18px]  px-2 ' href="/contactus">Contact us</Link></li>
-                            <li ><Link className='hover:text-[18px] px-2 ' href="/opertunities">Opertunities</Link></li>
+                            {/* Isme edit karo ye Link hai ek me use karoge 4 me atometic hoga  */}
+                            {Navbar.map((item) => (
+                                <li key={item.id} className='hover:text-[18px]  px-2 '>
+                                    <Link href={item.link}>{item.name}</Link>
+                                </li>
+                            ))}
                         </ul>
                     </div>
-                    <div> 
-                        <button>
-                           <Link className='bg-white  border-2 text-black rounded-full hover: p-2 h-2 ' href="/signup">Sign Up</Link>
-                        </button>
-                        <button>
-                            <Link className='bg-white text-black border-2 rounded-full h-2 hover: p-2 m-1 ' href="/login">login</Link>
-                        </button>
-                        
+                    <div>
+                        {/* Isme edit karo ye button hai ek me use karoge dusra atometic hoga  */}
+                        {
+                            btnname.map((item) => (
+                                <button className='h-10 p-1 m-1.5 w-28 text-[15px] font-bold bg-white rounded-[11px] hover:text-[21px]' key={item.id}>
+                                    <Link className='text-black h-10 w-[150px]' href={item.link}>{item.name}</Link>
+                                </button>
+                            ))
+                        }
                     </div>
-                </div> 
+                </div>
             </div>
 
 
