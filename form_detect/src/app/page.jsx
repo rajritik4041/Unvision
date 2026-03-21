281b28a6fcc92c9867b8ed790939c4ab27cae083
rajritik4041 <rajritik.9236@gmail.com>
2026-03-21 11:48:38 +0530

    Add a Next JS Structure

---
+++ form_detect/src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Main from "@/components/Guest/main";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <Main />
+      </main>
+    </>
+  );
+}
