0fe3742f7f3bda0751bf5dc545e9a4626c9432ec
rajritik4041 <rajritik.9236@gmail.com>
2026-03-22 16:00:16 +0530

    connect mongoose

--- form_detect/src/app/api/login/route.js
+++ form_detect/src/app/api/login/route.js
@@ -1,9 +1,20 @@
+import connectDB from "@/config/mongodb";
+import User from "@/models/user";
+
+
 export async function POST(req) {
-  const body = await req.json()
-  console.log(body)
+    await connectDB();
+    const body = await req.json()
+    const { email, password } = body
+    console.log(email, password)
+    const colection = await User.create({ email, password })
+    if (!colection) {
+        return res.render('partials/404', { message: "contact not found" })
+    }
+
+    console.log(body)
     return Response.json({
         success: true,
         message: "Login successful"
-    })  
+    })
 }
-    
--- form_detect/src/app/api/signup/route.js
+++ form_detect/src/app/api/signup/route.js
@@ -1,8 +1,13 @@
+import connectDB from "@/config/mongodb"
+import User from "@/models/user";
+
+
 export async function POST(req) {
+   await connectDB()
   const body = await req.json()
-
+  const { username, email, password } = body
   console.log(body)
-
+  const colection = await User.create({ username, email, password })
   return Response.json({
     success: true,
     message: "Signup successful"
