2f3a8894ce90a7e4649057a87ede0124b8532d21
rajritik4041 <rajritik.9236@gmail.com>
2026-03-24 18:41:25 +0530

    check Validation login page

--- form_detect/src/app/(components)/aboutus/page.jsx
+++ form_detect/src/app/(components)/aboutus/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function page() {
   return (
     <div><div>
-        This is a about Us page</div></div>
+        This is a abhdhjhjehjehjfjfjffkdfkkdfout Us page</div></div>
   )
 }
 
